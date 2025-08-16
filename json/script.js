e9aa42dd67753a99e45e19ddbe1701721fc15292
Agg069 <everythinguuceo@outlook.com>
2025-08-16 09:34:40 +0530

    Update script.js

--- json/script.js
+++ json/script.js
@@ -1,105 +1,96 @@
-let blob=0,link=0,use=0,uuid,val,IT=0,agg=0;
+"use strict";
+var blob,link,use=0,val,IT=0,agg=0,F;
 const t=e=>document.getElementById(e),fs=t('fs'),
-show=x=>t('sH').textContent='Progress: '+IT,
+show=()=>t('sH').textContent=IT+' or '+((IT/F.length)*100).toFixed(4),
 onc=f=>{
 var _FN,SW=x=>f.startsWith(x),mc='.mcpack';
-if(SW('aggverse')){
-_FN='AggVerse.mcworld';
-agg=1;
-}
-else if(SW('torch'))_FN='Torch'+mc;
-else if(SW('mobbattle'))_FN='MobBattle-BP'+mc;
-else if(SW('grenade'))_FN='Ｇｒｅｎａｄｅ－ＢＰ'+mc;
-else if(SW('redstone'))_FN='Redstone-BP'+mc;
-else if(SW('darkland'))_FN='DarkLand-RP'+mc;
-else if(SW('wasteland'))_FN='WasteLand-BP'+mc;
-else if(SW('glowshot'))_FN='Glow-Shot'+mc;
-else if(SW('bettercomb'))_FN='BetterCombat-BP'+mc;
-else _FN=f.split('/')[0]+mc;
+if(SW('aggverse'))_FN='AggVerse.mcworld',agg=1;
+else if(SW('torch'))_FN='Dynamic-Lighting';
+else if(SW('mobbattle'))_FN='MobBattle-BP';
+else if(SW('grenade'))_FN='Ｇｒｅｎａｄｅ－ＢＰ';
+else if(SW('redstone'))_FN='Redstone-BP';
+else if(SW('darkland'))_FN='DarkLand-RP';
+else if(SW('wasteland'))_FN='WasteLand-BP';
+else if(SW('glowshot'))_FN='Glow-Shot';
+else if(SW('bettercomb'))_FN='BetterCombat-BP';
+else if(SW('3dcr'))_FN='3DCrystal';
+else _FN=f.split('/')[0];
 if(!_FN.includes('.mcworld'))agg=0;
-t('fn').value=_FN;
-},obf=(mem=null)=>{
-val=Number(t('v').value);
-if(mem===null)return"";
-if(Array.isArray(mem))return JSON.stringify(obfArr(mem)).replaceAll('\\\\u','\\u');
-if(typeof mem=='object')return JSON.stringify(obfObj(mem)).replaceAll('\\\\u','\\u');
-},getOf=ty=>{
+t('fn').value=agg?_FN:_FN+mc;
+},obf=mem=>JSON.stringify(Array.isArray(mem)?obfArr(mem):obfObj(mem)).replaceAll('\\\\u','\\u'),
+getOf=ty=>{
 if(typeof ty=='string')return'str';
 if(Array.isArray(ty))return'arr';
 if(typeof ty=='object'&&ty!=null)return'obj';
-return typeof ty;
 },shf=a=>{for(let i=a.length-1;i>0;i--){const j=Math.floor(Math.random()*(i+1));[a[i],a[j]]=[a[j],a[i]]}return a;
 },obfStr=str=>{
-let res=[];
-for(let i=0;i<str.length;i++){
-if(str[i]=='\\'){res.push(str[i++],str[i]);continue;}
-if((crypto.getRandomValues(new Uint32Array(1))[0]/4294967295)<val)res.push(str[i]);
-else res.push('\\u'+str.charCodeAt(i).toString(16).padStart(4,'0'));
-}
+const res=[];
+for(var i=0;i<str.length;i++)res.push(Math.random()>val?str[i]:'\\u'+str.charCodeAt(i).toString(16).padStart(4,'0'));
 return res.join('');
 },obfArr=arr=>{
-let res=[];
-for(const item of arr){
-switch(getOf(item)){
-case'str':res.push(obfStr(item));break;
-case'obj':res.push(obfObj(item));break;
-case'arr':res.push(obfArr(item));break;
-default:res.push(item);
+const res=[];
+for(const I of arr){
+switch(getOf(I)){
+case'str':res.push(obfStr(I));break;
+case'obj':res.push(obfObj(I));break;
+case'arr':res.push(obfArr(I));break;
+default:res.push(I);
 }
 }
 return res;
 },obfObj=obj=>{
-let res={};
-for(const prop of shf(Object.keys(obj))){
-if(!obj.hasOwnProperty(prop)||typeof obj[prop]=='function')continue;
-const key=obfStr(prop);
-switch(getOf(obj[prop])){
-case'str':res[key]=obfStr(obj[prop]);break;
-case'obj':res[key]=obfObj(obj[prop]);break;
-case'arr':res[key]=obfArr(obj[prop]);break;
-default:res[key]=obj[prop];
+const res={};
+for(const K of shf(Object.keys(obj))){
+const X=obfStr(K);
+switch(getOf(obj[K])){
+case'str':res[X]=obfStr(obj[K]);break;
+case'obj':res[X]=obfObj(obj[K]);break;
+case'arr':res[X]=obfArr(obj[K]);break;
+default:res[X]=obj[K];
 }
 }
 return res;
-},regex=/loot_tables|scripts|functions|textures|\.png|\.tga|texts|trades|sounds|spawn_rules/,
-reg3=/"minecraft:block":\s*\{[\s\S]*\}|"particle_effect":\s*\{[\s\S]*\}|"minecraft:feature_rules":\s*\{[\s\S]*\}|"minecraft:geometry":\s*\[[\s\S]*\]|"animations":\s*\{[\s\S]*\}/,
-reg4=/[?*:\\|"<>]+/g,
-OPT={compression:"DEFLATE",compressionOptions:{level:0}},s=async()=>{
+},reg=/loot_tables|scripts|functions|textures|\.png|\.tga|texts|trades|sounds|spawn_rules/,
+Reg=/"minecraft:block":\s*\{[\s\S]*\}|"particle_effect":\s*\{[\s\S]*\}|"minecraft:feature_rules":\s*\{[\s\S]*\}|"minecraft:geometry":\s*\[[\s\S]*\]|"animations":\s*\{[\s\S]*\}/,
+reG=/[?*:\\|"<>]+/g,
+ReG=/\/[^?*:\\|"<>/]+$/,
+s=async()=>{
 if(use)return;
-const f=t('f').files;
-if(!f.length){
-alert('Select files.');
-return;}
+F=t('f').files;
+if(!F.length)return;
 use=1;
-const Path=agg?p=>p:p=>p.replace(/\/[^?*:\\|"<>/]+$/i,'/'+crypto.randomUUID().replace('-','').slice(0,12)+'.json');
-const fn=t('fn').value,zip=new JSZip();
-for(IT=0;IT<f.length;IT++){
-if(IT%20==0)await new Promise(r=>setTimeout(r,0));
-const fi=f[IT];
+val=+t('v').value;
+const $=Date.now(),fn=t('fn').value,zip=new JSZip();
+await Promise.all(Array.from(F).map(async(fi,i)=>{
+IT=i+1;
 let path=fi.webkitRelativePath;
 const sl=path.split('/');
 path=path.slice(sl[0].length+1);
-if(!fi.name.endsWith('.json')){
-zip.file(path,fi,OPT);continue;}
+if(!path.endsWith('.json')){
+zip.file(path,fi);return;}
 try{
 let txt=await fi.text();
-if(sl.length>2&&!regex.test(path))path=Path(path);
-if(!reg3.test(txt))txt=obf(JSON.parse(txt));
-zip.file(path.replace(reg4,'_'),txt,OPT);
-}catch(e){fs.innerHTML+='<hr><strong>'+path+'</strong>: '+String(e).replaceAll('&','&amp;').replaceAll('<','&lt;').replaceAll('>','&gt;');zip.file(path,fi,OPT);}
-}
-blob=await zip.generateAsync({type:'blob',mimeType:'application/octet-stream',compression:'DEFLATE',compressionOptions:{level:0}});
+if(!Reg.test(txt))txt=obf(JSON.parse(txt));
+if(sl.length>2&&!reg.test(path)&&!agg)path=path.replace(ReG,'/'+crypto.randomUUID().replace('-','').slice(0,12)+'.json');
+const P=path;
+path=path.replace(reG,'_');
+if(P!==path)fs.innerHTML+='<hr><strong style="color:#F90;"> •'+path+'</strong>: Sanitized '+P;
+zip.file(path,txt);
+}catch(e){fs.innerHTML+='<hr><strong style="color:red;"> •'+path+'</strong>: '+String(e).replaceAll('&','&amp;').replaceAll('<','&lt;').replaceAll('>','&gt;');zip.file(path,fi);return;}
+}));
+blob=await zip.generateAsync({type:'blob',mimeType:'application/octet-stream',compression:'STORE'});
 link=document.createElement('a');
 link.download=fn;
-link.textContent='Download if not downloaded '+fn;
+link.textContent='Download';
 link.href=URL.createObjectURL(blob);
 document.body.appendChild(link);
 link.click();
-use=0;
-}
-function st(){
-if(blob!==0)URL.revokeObjectURL(blob);
-if(link!==0)document.body.removeChild(link);
+const _=Date.now()-$,Q=_/F.length,W=Q/1000;
+t('p').textContent=`MS: ${_-2}, S:${_/1000}, Avg:${W.toFixed(4)}`;
+use=0;},st=()=>{try{
+URL.revokeObjectURL(blob);
+document.body.removeChild(link);
 use=agg=0;
+}catch(e){}
 }
-window.onerror=alert;
+window.onerror=E=>fs.innerHTML+='<hr><span style="color:#0EE;"> •'+String(E).replaceAll('&','&amp;').replaceAll('<','&lt;').replaceAll('>','&gt;')+'</span>';
